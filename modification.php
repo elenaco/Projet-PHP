4cc6d2bd0fb37d389f9a516e646ac6bd623a1097
elenaco <50236894+elenaco@users.noreply.github.com>
2019-05-17 13:38:06 +0200

    Add files via upload

--- modification.php
+++ modification.php
@@ -5,7 +5,39 @@
 	</head>
 	<body>
 	<?php
-	echo ">Mettre à jour le mot de passe</button>
+	echo "<h1> Mes infos </h1>
+	<div class=\"form-popup\" id=\"formModification\">
+		<form action='gestionModification.php' method='POST'>
+			<div>
+				<label>Nom :</label>
+				<input type='text' name='nom'>
+			</div>
+			<div>
+				<label>Prénom :</label>
+				<input type='text' name='prenom'>
+			</div>
+			<div>
+				<label>Pseudo :</label>
+				<input type='text' name='pseudo'>
+			</div>
+			<div>
+				<label>Date de naissance :</label>
+				<input type='date' name='date_naissance'>
+			</div>
+			<div>
+			   <label>Biographie :</label> </br>
+			   <textarea name='bio'></textarea>
+			</div>
+			<div>
+				<label>Mail :</label>
+				<input type='email' name='mail'>
+			</div>
+			<button type=\"submit\" class=\"btn\">Valider les modifications</button>
 		</form>
-	</div>\"";
+	</div>
+	<div>
+		<form action='motDePasse.php' method='POST'>
+			<button type=\"submit\" class=\"btn\">Mettre à jour le mot de passe</button>
+		</form>
+	</div>";
 ?>
